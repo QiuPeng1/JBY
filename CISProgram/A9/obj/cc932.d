32be97e859e9615a7d95c1a7f407f9314f44ec06
QiuPeng1 <904505800@qq.com>
2018-10-16 22:09:06 +0800

    rm A9

--- CISProgram/A9/obj/cc932.d
+++
@@ -1,4 +0,0 @@
-.\obj\cc932.o: ..\FATFS\FatFs11\option\cc932.c
-.\obj\cc932.o: ..\FATFS\FatFs11\option\../ff.h
-.\obj\cc932.o: ..\FATFS\FatFs11\option\../integer.h
-.\obj\cc932.o: ..\FATFS\FatFs11\option\../ffconf.h
